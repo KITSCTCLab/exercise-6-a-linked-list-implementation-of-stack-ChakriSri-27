0909cd38c9a5b860174a577e4cae75945ee7a04b
ChakriSri-27 <91770676+ChakriSri-27@users.noreply.github.com>
2022-09-07 21:58:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,35 +2,46 @@ class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
-
+#     self.top=None
 
 class Stack:
   def __init__(self):
-    self.head = None
-
-  def push(self, data):
-        if self.head is None:
-            self.head = Node(data)
-        else:
-            new_node = Node(data)
-            new_node.next = self.head
-            self.head = new_node
- 
-    def pop(self):
-        if self.head is None:
-            return None
-        else:
-            popped = self.head.data
-            self.head = self.head.next
-            return popped
-
- # def status(self):
+    self.top = None
+    #self.top=None
+  def push(self, data) -> None:
+    # Write your code here
+    new=Node(data)
+    new.data=data
+    new.next=self.top
+    self.top=new
+  def pop(self) -> None:
+    # Write your code here
+    if self.top==None:
+      return None
+    else:
+      #temp=Node(data)
+      temp=self.top
+      self.top=self.top.next
+      temp.next=None
+      return temp
+      
+      
+  def status(self):
     """
     It prints all the elements of stack.
     """
-  
-
-
+    # Write your code here  
+    if self.top==None:
+      print("None")
+    else:
+      #ptr=Node(data)
+      ptr=self.top
+      while(ptr!=None):
+        print(ptr.data,end="")
+        print("=>",end="")
+        ptr=ptr.next
+        if(ptr==None):
+          print("None")
 # Do not change the following code
 stack = Stack()
 operations = []
